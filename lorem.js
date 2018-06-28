5281b616ff77297164157abf2ea403445d6526c1
Git-Good <gcui@sfu.ca>
2018-06-28 16:22:58 -0700

    Removed dependencies for jQuery, HTML, CSS
    Purely javascript - add script file to html and run.
    
    Undo button implemented (previous action)

--- lorem.js
+++ lorem.js
@@ -1,41 +1,104 @@
 //$(document).ready(() => {})
+//create container for buttons
+//var container = document.createElement("div")
+//container
+/*container.style.position = "absolute"
+container.style.right = 0
+container.style.top = 0
+container.style.display = "inline-block"*/
+
+//Buttons currently rely on 1st div in the HTML document for positioning
+
 //Create button for text randomization
 var button = document.createElement("button");
 button.innerHTML = "Obfuscate Text";
+/*button.style.position = "absolute"
+button.style.display = "inline-block"
+button.style.right = 0
+button.style.top = 0 */
 
 //Append 
-var body = document.getElementsByTagName("body")[0];
+var body = document.getElementsByTagName("div")[0];
 body.appendChild(button);
+
+//container.append(button)
 
 //Add event handler
 button.addEventListener ("click", function() {
     randomizeText();
-   
+
+    if (storage !== []){
+        button2.style.display = "inline-block"
+    //button2.style.visibility = 'visible';
+    }
 });
 
+
+var button2 = document.createElement("button");
+button2.innerHTML = "Undo";
+button2.style.position = "absolute"
+//button2.style.right = 0
+//button2.style.top = 0
+
+//Append 
+var body2 = document.getElementsByTagName("div")[0];
+body.appendChild(button2);
+
+button2.style.display = 'none';
+//button2.style.visibility = 'hidden';
+
+//Add event handler
+button2.addEventListener ("click", function() {
+    unrandomizeText();
+    button2.style.display = 'none';
+    //button2.style.visibility = 'hidden';
+
+});
+
+//creates div to append message to, even if none exists
+var hud = document.createElement("div")
+hud.id = "dis"
+hud.style.position = 'absolute'
+//hud.style.right = 0
+//hud.style.top = 0
+document.body.appendChild(hud)
+
+//css style - used for annotation element id  
+var style = document.createElement('style');
+style.type = 'text/css';
+style.innerHTML = "#annotation {position: fixed; top: 5px; right: 5px; padding: 3px; background-color: rgba(255, 255, 255, 0.63);font-family: sans-serif;font-size:  20px;z-index: 5000;color: #E3E3E3; background: #000;}"
+document.getElementsByTagName('head')[0].appendChild(style);
 
 var tag = ""
 //select elements with specified tag
 var element = document.getElementsByTagName(tag)
+var message = document.getElementById("dis")
+
 document.addEventListener("dblclick", function (evt) {
+    storage =[]
     var elem
     if (evt.srcElement)  elem = evt.srcElement;
     else if (evt.target) elem = evt.target;
 
-    
     tag = elem.tagName.toLowerCase();
 
     console.log(typeof tag)
-    $('#log').children('div').remove()
-            
-    message = $('#log').append("<div id = 'annotation'>" +tag+ "</div>");    
-
-    //console.log(inspect)
-     message.show();
     
-    // message.delay(1500).fadeOut(500);
+    /*jQuery Version
+    $('#dis').children('div').remove()
+    message = $('#dis').append("<div id = 'annotation'>" +tag+ "</div>");    
+    message.show();
+    
+    //message.delay(1500).fadeOut(500);
+    */
+    
+    while (message.hasChildNodes()){
+        message.removeChild(message.firstChild)}
+
+    message.innerHTML += "<div id = 'annotation'>" +tag+ "</div>"
     
     console.log(tag)
+    //select element type by double-clicked tag 
     element = document.getElementsByTagName(tag)
 })
 
@@ -63,30 +126,48 @@ console.log(element.item(0).textContent)*/
 
 //global variable to store previous text
 var storage = []
+var tagHist
 
 function randomizeText (){
-    console.log(element.item(0).textContent)
-    var itemCount 
-    for (itemCount = 0; itemCount < element.length; itemCount++){
-        var loremipsum = ""
-        var index
-        //Store previous text in array
-        storage[itemCount] = element.item(itemCount).textContent
-        //strlength is equal to the HTMLCollection.item's text content's word count, assuming words are seperated by spaces
-        var strlength = element.item(itemCount).textContent.split(" ").length
-        //generate a lorem ipsum string with variable length 
-        for (index = 0; index<strlength; index++){
-            loremipsum = words[getRandomPosIntRange(rmin, rmax)] + " " + loremipsum 
+    //console.log(/^h/.test(tag))
+    if (tag === "p" || /^h[1-6]/.test(tag)){
+        console.log(element.item(0).textContent)
+        var itemCount 
+        for (itemCount = 0; itemCount < element.length; itemCount++){
+            var loremipsum = ""
+            var index
+            //Store previous text in array
+            storage[itemCount] = element.item(itemCount).innerHTML
+            //strlength is equal to the HTMLCollection.item's text content's word count, assuming words are seperated by spaces
+            var strlength = element.item(itemCount).textContent.split(" ").length
+            //generate a lorem ipsum string with variable length 
+            for (index = 0; index<strlength; index++){
+                loremipsum = words[getRandomPosIntRange(rmin, rmax)] + " " + loremipsum 
+            }
+            //create new element of specified type, replace the previous element with new element (contains same word count)
+            var newElement = document.createElement(tag)
+            //tagHist = tag
+            newElement.innerHTML = loremipsum.trim()
+            element.item(itemCount).parentNode.replaceChild(newElement, element.item(itemCount))
+
+            //return true
         }
-        //create new element of specified type, replace the previous element with new element (contains same word count)
-        var newElement = document.createElement(tag)
-        newElement.innerHTML = loremipsum.trim()
-        element.item(itemCount).parentNode.replaceChild(newElement, element.item(itemCount))
-    }
+    } else message.innerHTML +="<div id = 'annotation'>Select a Text Element</div>"
+    /*jQuery version
+    message = $('#dis').append("<div id = 'annotation'>" +"Select a Text Element"+ "</div>");    
+    message.show();
+    */
 }
 
-
-
+function unrandomizeText () {
+    console.log(storage)
+    for (let i = 0; i < storage.length; i++){
+        let newElement = document.createElement(tag)
+        newElement.innerHTML = storage[i]
+        element.item(i).parentNode.replaceChild(newElement, element.item(i))
+    }
+    storage = []
+}
 
 
 //console.log(loremipsum)
